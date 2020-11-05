ebae52aaa08980aeee42d6ade469e3fc359fc0f6
ryo-n <ryo.nakamura0401@gmail.com>
2020-11-06 00:24:48 +0900

    進捗

---
+++ problems/10.py
@@ -0,0 +1,10 @@
+
+def get_primes(n):
+    sieve = [1] * n
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if sieve[i]:
+            sieve[i * i:: 2 * i] = [0] * ((n - i * i - 1) // (2 * i) + 1)
+    return [2] + [i for i in range(3, n, 2) if sieve[i]]
+
+
+print(sum(get_primes(2000000)))
